bdda55afa008f594812e846128d5989ed93f12de
thanhnt <thanhnt@omt.com.vn>
2022-04-15 00:30:16 +0700

    Doing tesst

--- tests/Browser/LoginTest.php
+++ tests/Browser/LoginTest.php
@@ -8,16 +8,21 @@ use Tests\DuskTestCase;
 
 class LoginTest extends DuskTestCase
 {
+
     /**
      * A Dusk test example.
      *
      * @return void
      */
-    public function testExample()
+    public function testLogin()
     {
-        $this->browse(function (Browser $browser) {
-            $browser->visit('/admin')
-                    ->assertSee('Laravel');
+        $this->browse(function(Browser $browser) {
+            $browser->visit('/admin/auth/login')
+                ->type('username', "admin")
+                ->type('password', "admin")
+                ->click('button[type="submit"]')
+                ->assertPathIs("admin");
         });
     }
+
 }
